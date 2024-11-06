7ef87b4c87bf1611f91237329351816117b141aa
Esme <esmeralda.lunaromano14@mail.com>
2024-11-06 10:27:24 -0600

    Ejercicio 03

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+"""
+Programa que leé un número e indica si es par o impar
+"""
+a = int(input("Ingresa un número natural cualquiera: "))
+if a % 2 == 0:
+    print("El número ingresado es par")
+else:
+    print("El número ingresado es impar")
+print("Fin del programa")
---
+++ ejercicio6.py
@@ -0,0 +1,5 @@
+"""
+Programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
+"""
+a= input("Ingresa una cadena:")
+if a == 
