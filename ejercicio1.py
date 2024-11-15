3a7862246e2496abe33a49edae653ec0a8b1f9b4
Esme <esmeralda.lunaromano14@mail.com>
2024-11-15 12:54:38 -0600

    Ejercicio 01

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,6 +5,6 @@ print("Programa que pide dos números e indica cuál es el mayor")
 num1 =int(input("Ingresa el primer número: "))
 num2 = int(input("Ingresa el segundo número: "))
 if num1 > num2:
-    print("El número mayor es", num1)
+    print("El número mayor es: ", num1)
 else:
-     print("El número mayor es", num2)
+     print("El número mayor es: ", num2)
